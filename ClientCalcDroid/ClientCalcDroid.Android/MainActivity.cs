39c6fb03d52b8833e687192cf5376bdd7917d211
Roma121212 <chuprina.r.v@gmail.com>
2017-11-07 16:09:07 +0300

    18, УБРАЛ НЕ НУЖНЫЕ ФАЙЛЫ

--- ClientCalcDroid/ClientCalcDroid.Android/MainActivity.cs
+++ ClientCalcDroid/ClientCalcDroid.Android/MainActivity.cs
@@ -1,35 +1,91 @@
 ﻿using System;
-
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
+using static Android.Views.View;
+using System.Net.Http;
 
 namespace ClientCalcDroid.Droid
 {
-	[Activity (Label = "ClientCalcDroid.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity (Label = "ClientCalcDroid.Android", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+        System.Net.Http.HttpClient httpC;
 
-		protected override void OnCreate (Bundle bundle)
+        protected override void OnCreate (Bundle bundle)
 		{
+            httpC = new System.Net.Http.HttpClient();
 			base.OnCreate (bundle);
+            SetContentView(Resource.Layout.Main);
+            Button[] buttonNum = { FindViewById<Button>(Resource.Id.button7), FindViewById<Button>(Resource.Id.button8), FindViewById<Button>(Resource.Id.button9),
+            FindViewById<Button>(Resource.Id.button4), FindViewById<Button>(Resource.Id.button5), FindViewById<Button>(Resource.Id.button6), FindViewById<Button>(Resource.Id.button1),
+            FindViewById<Button>(Resource.Id.button2), FindViewById<Button>(Resource.Id.button3), FindViewById<Button>(Resource.Id.buttondot), FindViewById<Button>(Resource.Id.button0)};
+            for(int i = 0; i < buttonNum.Length; i++)
+            {
+                buttonNum[i].Click += new EventHandler(ClicBtn);
+            }
+            Button[] buttonOpr = { FindViewById<Button>(Resource.Id.add), FindViewById<Button>(Resource.Id.riz), FindViewById<Button>(Resource.Id.mult), FindViewById<Button>(Resource.Id.div) };
+            for (int i = 0; i < buttonOpr.Length; i++)
+            {
+                buttonOpr[i].Click += new EventHandler(Opr_Click);
+            }
+            Button buttonC = FindViewById<Button>(Resource.Id.buttonC);
+            buttonC.Click += new EventHandler(AllClear_Click);
+            Button buttonClearOne = FindViewById<Button>(Resource.Id.cler);
+            buttonClearOne.Click += new EventHandler(button18_Click);
+            Button buttonRes = FindViewById<Button>(Resource.Id.res);
+            buttonRes.Click += new EventHandler(Res_Click);
+        }
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+        
+        string fNum;
+        string sNum;
+        string op;
+        string url = "http://localhost:8080/";
+        private void ClicBtn(object s, EventArgs e)
+        {
+            FindViewById<TextView>(Resource.Id.textView1).Text += ((Button)s).Text;
+        }
+        private void Opr_Click(object sender, EventArgs e)
+        {
+            if (FindViewById<TextView>(Resource.Id.textView1).Text != "")
+            {
+                fNum = FindViewById<TextView>(Resource.Id.textView1).Text;
+                op = ((Button)sender).Text;
+                if (op == "+") { op = "plus"; }
+                FindViewById<TextView>(Resource.Id.textView1).Text = "";
+            }
+        }
+        private async void Res_Click(object sender, EventArgs e)
+        {
+            if (FindViewById<TextView>(Resource.Id.textView1).Text != "")
+            {
+                sNum = FindViewById<TextView>(Resource.Id.textView1).Text;
+                try
+                {
+                    FindViewById<TextView>(Resource.Id.textView1).Text = await httpC.GetStringAsync($"{url}?num1={fNum}&num2={sNum}&opr={op}");
+                }
+                catch (HttpRequestException)
+                {
+                    FindViewById<TextView>(Resource.Id.textView1).Text = "server not respond";
+                }
+            }
+        }
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+        private void AllClear_Click(object sender, EventArgs e)
+        {
+            fNum = sNum = op = null;
+            FindViewById<TextView>(Resource.Id.textView1).Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (FindViewById<TextView>(Resource.Id.textView1).Text != "")
+            {
+                FindViewById<TextView>(Resource.Id.textView1).Text = FindViewById<TextView>(Resource.Id.textView1).Text.Remove(FindViewById<TextView>(Resource.Id.textView1).Text.Length - 1, 1);
+            }
+        }
+    }
 }
 
 
--- ClientCalcDroid/ClientCalcDroid.Android/Resources/Resource.designer.cs
+++ ClientCalcDroid/ClientCalcDroid.Android/Resources/Resource.designer.cs
@@ -60,8 +60,68 @@ namespace ClientCalcDroid.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050006
+			public const int add = 2131034118;
+			
+			// aapt resource value: 0x7f050010
+			public const int button0 = 2131034128;
+			
+			// aapt resource value: 0x7f05000b
+			public const int button1 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int button2 = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int button3 = 2131034125;
+			
+			// aapt resource value: 0x7f050007
+			public const int button4 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int button5 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int button6 = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int button7 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int button8 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int button9 = 2131034117;
+			
+			// aapt resource value: 0x7f050011
+			public const int buttonC = 2131034129;
+			
+			// aapt resource value: 0x7f05000f
+			public const int buttondot = 2131034127;
+			
+			// aapt resource value: 0x7f050013
+			public const int cler = 2131034131;
+			
+			// aapt resource value: 0x7f050012
+			public const int div = 2131034130;
+			
+			// aapt resource value: 0x7f050002
+			public const int gridLayout1 = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f05000e
+			public const int mult = 2131034126;
+			
+			// aapt resource value: 0x7f050014
+			public const int res = 2131034132;
+			
+			// aapt resource value: 0x7f05000a
+			public const int riz = 2131034122;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
 			
 			static Id()
 			{
